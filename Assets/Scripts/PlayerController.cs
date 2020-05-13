b385c29e6d6a475ea9019862d34695957c3f41c7
Diego Estrada <est.diego03@gmail.com>
2020-05-12 23:41:56 -0600

    working on cam

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+   
