20c4ecfece1e6a39a060cc946a68e40d7c0214f1
Diego Estrada <est.diego03@gmail.com>
2020-05-14 21:50:58 -0600

    move and jump working

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 5f;
+    public Rigidbody rb;
+    public LayerMask groundLayers;
+    public float jumpforce = 7f;
+    public CapsuleCollider col;
+
+    // Start is called before the first frame update
+    private void start()
+    {
+        rb = GameObject.Find("Player").GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+
+    }
+
+    void PlayerMovement()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up*jumpforce, ForceMode.Impulse);
+        }
+        
+
+
+    }
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+}
