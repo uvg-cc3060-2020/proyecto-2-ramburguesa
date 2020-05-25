111783496590671b18f577a56242cbc1a4e3e8bc
eli17048 <andrea.elias16@gmail.com>
2020-05-24 23:32:29 -0600

    disparo

---
+++ Assets/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField]
+    private int startingHealth = 5;
+    private int currentHealth;
+
+    // Start is called before the first frame update
+    private void onEnable()
+    {
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/raycastShoot.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class raycastShoot : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(0.5f, 1.5f)]
+    private float fireRamge = 1;
+
+    [SerializeField]
+    [Range(1f, 10f)]
+    private int damage = 1;
+
+    private float timer;
+
+    [SerializeField]
+
+    private Transform face;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer>= fireRamge)
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                timer = 0f;
+                FireRun();
+
+            }
+        }
+       
+    }
+
+    private void FireRun()
+    {
+        Debug.DrawRay(face.position, face.forward * 100, Color.red, 0.5f);
+
+        Ray ray = new Ray(face.position, face.forward);
+        RaycastHit hitinfo;
+        
+        if(Physics.Raycast(ray, out hitinfo, 100))
+        {
+            var health =hitinfo.collider.GetComponent<Health>();
+
+            if(health != null)
+            {
+                health.TakeDamage(damage);
+
+            }
+        }
+    }
+
+   
+}
